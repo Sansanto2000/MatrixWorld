561543d78a7eea79519b2ef13ff7cae858ee7d15
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2024-12-22 19:27:31 -0300

    :sparkles: Incorporacion de diccionario con instancia de clase

--- MatrixWorld/Assets/Scripts/PieceDict.cs
+++ MatrixWorld/Assets/Scripts/PieceDict.cs
@@ -1,17 +1,30 @@
+using System.Collections.Generic;
+
 public class PieceData
 {
     public int code { get; }    
     public char small { get; }
+    public string name { get; }
 
-    public PieceData(int code, char small)
+    public PieceData(int code, char small, string name)
     {
         this.code = code;
         this.small = small;
+        this.name = name;
     }
 }
 
-public static class WorldPiece
+public class PieceDict
 {
-    public static readonly PieceData Void = new PieceData(0, '_');
-    public static readonly PieceData Player = new PieceData(1, 'P');
+    public static readonly PieceData Void = new PieceData(0, '_', "Void");
+    public static readonly PieceData Player = new PieceData(1, 'P', "Player");
+
+    Dictionary<int, PieceData> dict;
+
+    public PieceDict()
+    {
+        this.dict = new Dictionary<int, PieceData>();
+        this.dict.Add(0, PieceDict.Void);
+        this.dict.Add(1, PieceDict.Player);
+    }
 }
