1da2f582346738d378e5af1d2c1a9914fbe8bfae
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-01-11 22:03:55 -0300

    ✨ Renderizado de escaleras en el mundo

--- MatrixWorld/Assets/Scripts/PieceDict.cs
+++ MatrixWorld/Assets/Scripts/PieceDict.cs
@@ -21,6 +21,7 @@ public class PieceDict
     public static readonly PieceData Void = new PieceData(0, '_', "Void");
     public static readonly PieceData Player = new PieceData(1, 'P', "Player");
     public static readonly PieceData Wall = new PieceData(2, 'W', "Wall");
+    public static readonly PieceData Stair = new PieceData(3, 'S', "Stair");
 
     Dictionary<int, PieceData> dict;
 
@@ -30,6 +31,7 @@ public class PieceDict
         this.dict.Add(0, PieceDict.Void);
         this.dict.Add(1, PieceDict.Player);
         this.dict.Add(2, PieceDict.Wall);
+        this.dict.Add(3, PieceDict.Stair);
         for (int i = 0; i < tiles.Length; i++)
         {
             this.dict[i].tile = tiles[i];
