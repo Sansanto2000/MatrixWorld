8635bad1d1dfb3235a23e97d5247dd566c25c306
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-02-23 15:58:34 -0300

    ♻️ Modularizado de lógica de emisión de partículas.

--- MatrixWorld/Assets/Scripts/Hittable.cs
+++ MatrixWorld/Assets/Scripts/Hittable.cs
@@ -18,25 +18,28 @@ public class Hittable : MonoBehaviour
         hitEffect = GetComponent<ParticleSystem>();
     }
 
-    public void Hit(float damage, Vector3 hitterPosition)
-    {
-        
-        life -= damage;
-        if (life <= 0) {
-            hitEffect.Play();
-            audioSource.Play();
-            gameMaster.EnemyDeleted(gameObject);
-            Destroy(gameObject);
-            return;
-        }
-
+    void emitParticles(Vector3 hitterPosition) {
+        /// <summary>
+        /// Emite partículas en la dirección opuesta del que realiza el golpe
+        /// </summary>
         float diffx = hitterPosition.x - hitEffect.transform.position.x;
         float diffy = hitterPosition.y - hitEffect.transform.position.y;
         float angle = Mathf.Atan2(diffy, diffx) * Mathf.Rad2Deg;
         float fixAngle = angle + 90;
         hitEffect.transform.rotation = Quaternion.Euler(0f, 0f, fixAngle);
-
         hitEffect.Play();
+    }
+
+    public void Hit(float damage, Vector3 hitterPosition)
+    {
+        emitParticles(hitterPosition);
         audioSource.Play();
+        
+        life -= damage;
+        if (life <= 0) {
+            gameMaster.EnemyDeleted(gameObject);
+            Destroy(gameObject);
+            return;
+        }
     }
 }
